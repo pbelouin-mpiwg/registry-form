a3c6372bc374cf1be726d893ed681cdb61c1b88e
pascal.belouin <pbelouin@mpiwg-berlin.mpg.de>
2021-07-19 13:17:49 +0200

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,99 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <formulate-form v-model="values">
+      <FormulateInput type="text" name="title" label="Title" validation="required" />
+      <FormulateInput
+        type="radio"
+        label="Creator Type"
+        name="creatorType"
+        :options="{
+          person: 'Person',
+          organisation: 'Organisation'
+        }"
+      />
+      <FormulateInput
+        name="creatorName"
+        validation="required"
+        label="Creator Name"
+      />
+      <FormulateInput
+        type="checkbox"
+        name="publisherDifferentFromCreator"
+        label="Publisher is different from the Creator"
+      />
+      <FormulateInput
+          type="group"
+          name="publisher"
+          v-if="values.publisherDifferentFromCreator"
+      >
+        <FormulateInput
+          type="radio"
+          label="Publisher Type"
+          name="publisherType"
+          :options="{
+            person: 'Person',
+            organisation: 'Organisation'
+          }"
+        />
+        <FormulateInput
+          name="publisherName"
+          validation="required"
+          label="Publisher Name"
+        />
+      </FormulateInput>
+      <FormulateInput
+        name="contactPoint"
+        validation="required"
+        label="Contact Point"
+      />
+      <FormulateInput
+        name="description"
+        type="textarea"
+        validation="required"
+        label="Description"
+      />
+
+      <vue-tags-input
+        v-model="tag"
+        :tags="tags"
+        @tags-changed="newTags => tags = newTags"
+      />
+      <FormulateInput type="submit" label="Save" />
+      <pre>{{ values }}</pre>
+    </formulate-form>
   </div>
 </template>
 
 <script>
+import Vue from 'vue'
+import VueFormulate from '@braid/vue-formulate'
+import VueTagsInput from '@johmun/vue-tags-input'
+Vue.use(VueFormulate)
+
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  components: {
+    VueTagsInput,
+  },
+  data: function () {
+    return {
+      values: {},
+      tags: [],
+      tag: ''
+    }
+  },
+  computed: {
+    simpleStringArray() {
+      return this.tags.map(tag => tag.text);
+    }
+  },
+  created() {
+    this.values.themes = this.simpleStringArray;
+  },
+  watch: {
+    simpleStringArray() {
+      this.values.themes = this.simpleStringArray;
+    }
   }
 }
 </script>
